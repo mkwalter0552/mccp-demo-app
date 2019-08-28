9cf3f81094d77aec574ace5fec7660a8d4788ab2
Eric Morter <morter.eric@mayo.edu>
2019-08-28 16:12:33 -0500

    Changes to database configuration

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -7,18 +7,30 @@ provider "google" {
 
 # create a database server running MySQL 5.7
 resource "google_sql_database_instance" "mysql" {
-  name = "mysql57"
+  name = "mysql-5-7"
   database_version = "MYSQL_5_7"
 
   settings {
     # run on a small, shared-core machine for demo purposes
     tier = "db-f1-micro"
+    ip_configuration {
+        # authorized_networks: [
+        #   {
+        #     name ="all-open",
+        #     value = "0.0.0.0/0"
+        #   }
+        # ],
+        ipv4_enabled = true
+        require_ssl = false
+      }
   }
 }
 
 # create a user in the database server
 resource "google_sql_user" "user" {
-  name     = "database_user"
+  name = "database_user"
+  # hosts this user is allowed to connect from, % means everywhere
+  host = "%"
   password = "${var.db_user_password}"
   instance = "${google_sql_database_instance.mysql.name}"
 }
